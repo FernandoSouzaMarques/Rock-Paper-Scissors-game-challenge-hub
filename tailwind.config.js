7e755c17e5021f6ca68aec43b2081ca61b41bcd4
novahaus <fernandinhosouzamarques@gmail.com>
2022-03-14 09:38:29 -0300

    feat: add stack on project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './source/presentation/pages/**/*.{js,ts,jsx,tsx}',
+    './source/presentation/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
